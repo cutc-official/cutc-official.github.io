1b02084f2b582b963cc4f6e3efb37a2de1c5c4c6
Daniel Raymond <dan@raymond.ch>
2021-01-29 18:31:06 -0500

    Added Countdown

---
+++ src/components/homePage/Countdown.vue
@@ -0,0 +1,29 @@
+<template>
+	<div id="countdown">
+		{{ timeStamp }}
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'Countdown',
+	data: () => ({
+		timeStamp: 0
+	}),
+	methods: {
+		updateTimeStamp() {
+			// Date objects...
+		}
+	}
+}
+</script>
+
+<style scoped>
+#countdown {
+	width: 100%;
+	padding: 1em;
+	box-sizing: border-box;
+
+	border: 1px solid black;
+}
+</style>
--- src/components/homePage/Hero.vue
+++ src/components/homePage/Hero.vue
@@ -1,34 +1,42 @@
 <template>
-<div class="intro" id="container">
-	<img src="../../assets/building.svg" id="cutc-building-image">
-	<!-- TEXT COLUMN -->
-	<span>
-		<h3>Welcome to the 2021</h3>
-		<h1>Canadian Undergraduate Technology Conference</h1>
-		<!-- <h2>Building our Future</h2> -->
-		<div style="height: 1em"/>
+<div id="hero">
+	<div class="intro">
+		<img src="../../assets/building.svg" id="cutc-building-image">
+		<!-- TEXT COLUMN -->
+		<span>
+			<h3>Welcome to the 2021</h3>
+			<h1>Canadian Undergraduate Technology Conference</h1>
+			<!-- <h2>Building our Future</h2> -->
+			<div style="height: 1em"/>
 
-		<h3>Jul 24-25, 2021 • Virtual Event</h3>
+			<h3>Jul 24-25, 2021 • Virtual Event</h3>
 
-		<!-- Begin Mailchimp Signup Form -->
-		<form action="https://cutc.us10.list-manage.com/subscribe/post?u=73d566326ae6bb629b42fb5fd&amp;id=e28a4a51fe" method="post" 
-				target="_blank" novalidate>
-			<input type="email" name="EMAIL" placeholder="Enter your email for updates!" required>
-			<!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-			<div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_73d566326ae6bb629b42fb5fd_e28a4a51fe" tabindex="-1" value=""></div>
-			<input type="submit" value="Submit">
-		</form>
-		<!-- End Mailchimp Signup Form -->
+			<!-- Begin Mailchimp Signup Form -->
+			<form action="https://cutc.us10.list-manage.com/subscribe/post?u=73d566326ae6bb629b42fb5fd&amp;id=e28a4a51fe" method="post" 
+					target="_blank" novalidate>
+				<input type="email" name="EMAIL" placeholder="Enter your email for updates!" required>
+				<!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+				<div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_73d566326ae6bb629b42fb5fd_e28a4a51fe" tabindex="-1" value=""></div>
+				<input type="submit" value="Submit">
+			</form>
+			<!-- End Mailchimp Signup Form -->
 
-		<!-- LINKS -->
-		<a href="https://2020.cutc.ca" target="_blank">Check out CUTC 2020</a>
-	</span>
+			<!-- LINKS -->
+			<a href="https://2020.cutc.ca" target="_blank">Check out CUTC 2020</a>
+		</span>
+	</div>
+	<countdown/>
 </div>
 </template>
 
 <script>
+import Countdown from './Countdown.vue';
+
 export default {
 	name: 'Hero',
+	components: {
+		Countdown
+	},
 	data: () => ({
 		email: '',
 		signup: true
@@ -37,15 +45,16 @@ export default {
 </script>
 
 <style scoped>
+#hero {
+	background: linear-gradient(180deg, #3C0805 0%, rgba(103, 8, 3, 0.8) 100%);
+}
 .intro {
 	display: grid;
 	grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
 	align-items: center;
-	height: 50%;
 	column-gap: 1em;
 	padding: 10% var(--splash-page-inset);
 	box-sizing: border-box;
-	background: linear-gradient(180deg, #3C0805 0%, rgba(103, 8, 3, 0.8) 100%);
 }
 #cutc-building-image {
 	max-height: 50vh;
