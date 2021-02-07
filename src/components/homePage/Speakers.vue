712a7f783b7b36233e152f9f620c7c0c98cf5756
angel <li.angel.d02@gmail.com>
2021-02-07 16:47:06 -0500

    speaker carousel

--- src/components/homePage/Speakers.vue
+++ src/components/homePage/Speakers.vue
@@ -1,26 +1,36 @@
 <template>
 <div id="speakers">
 	<h2>CUTC 2020</h2>
-	<div id="TABS">
+	<div id="tabs">
 		<p id="tab" @click="currentPage = 0"> Speakers </p>
 		<p id="tab" @click="currentPage = 1"> Events </p>
 	</div>
-	<div id="content">
-		<p v-if="currentPage == 0"> speaker info </p>
-		<p v-else> event info </p>
-	</div>
+	<vueper-slides v-if="currentPage == 0">
+		<vueper-slide v-for="i in 5" :key="i" :title="i.toString()" />
+	</vueper-slides>
+	<vueper-slides v-else>
+		<vueper-slide v-for="i in 5" :key="i" :title="i.toString()" />
+	</vueper-slides>
 </div>
 </template>
 
-
 <script>
+import { VueperSlides, VueperSlide } from 'vueperslides'
+import 'vueperslides/dist/vueperslides.css'
+
 export default {
+	components: { VueperSlides, VueperSlide },
 	name: 'Speakers',
-	data () {
-		return {
+	data: () => ({
+		slides: [
+			{
+				title: 'Slide #1',
+				content: 'Slide content.'
+				}
+				],
 		currentPage: 0,
-		}
-	}
+}),
+	
 }
 </script>
 
@@ -29,7 +39,7 @@ export default {
 	margin: 1em;
 	background: var(--nav-color);
 }
-#TABS {
+#tabs {
 	margin: 0 auto;
 	display: flex;
 }
