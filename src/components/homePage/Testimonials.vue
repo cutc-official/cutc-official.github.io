2e2f3b1624f90951a0174b4da3f8e47e9d847263
Sanaa Syed <sanaa@Sanaas-MacBook-Pro.local>
2021-03-08 13:54:25 -0500

    Adding mobile css.

--- src/components/homePage/Testimonials.vue
+++ src/components/homePage/Testimonials.vue
@@ -1,20 +1,23 @@
 <template>
   <div id="testimonials">
-    <h2>Testimonials</h2>
-    <!-- SIDE SCROLLER -->
     <div>
       <img src="../../assets/testimonial_building.svg" id="building" />
       <vueper-slides class="no-shadow" :bullets="false" :arrows="false" autoplay>
         <vueper-slide 
           v-for="(slide, i) in information['slides']" 
-          :key="i"  
+          :key="i"
         >
           <template v-slot:content>
             <div class="vueperslide__content-wrapper" style="flex-direction: row">
               <div id="slides">
-                <img src="../../assets/pic.svg" style="float: left; margin: 7% 10%;"/>
-                <p style="margin: 10% 0 2%;">{{ slide.title }}</p>
-                <span style="margin: 2%;">{{ slide.content }}</span>
+                <div id="title">
+                <img :src="slide.image" id="person" />
+                  <br />
+                  <strong> {{ slide.name }} </strong>
+                  <br/> 
+                  <strong> {{ slide.personDescription }} </strong>
+                </div>
+                <div id="content">{{ slide.content }}</div>
               </div>
             </div>
           </template>
@@ -37,6 +40,13 @@ export default {
     return {
       information: TestimonialInfo
     };
+  },
+  methods: {
+    getImage() {
+      const jsonObjects = TestimonialInfo['slides'];
+      console.log("here")
+      document.getElementById("displayPic").src = jsonObjects[0].image;
+    }
   }
 }
 </script>
@@ -45,6 +55,9 @@ export default {
 #testimonials {
 	margin: 1em;
   color: black;
+  font-family: Montserrat;
+  font-style: normal;
+  font-weight: bold;
 }
 
 #building {
@@ -53,10 +66,148 @@ export default {
 }
 
 #slides {
-  width: 60%;
+  width: 50%;
   height: 50%;
   background: #FEEAEA;
   float: right; 
-  color: black;
+}
+
+#title {
+  float: left;
+  margin-top: 5%;
+  margin-left: 5%;
+  margin-right: 6%;
+}
+
+#content {
+  margin: 8%;
+  text-align: left;
+}
+
+@media screen and (max-width:1450px) {
+  #slides {
+    width: 50%;
+    height: 60%;
+  }
+}
+
+@media screen and (max-width:1310px) {
+  #building {
+    height: 30em;
+  }
+
+  #slides {
+    width: 60%;
+    height: 70%;
+  }
+}
+
+@media screen and (max-width:1110px) {
+  #building {
+    height: 25em;
+  }
+
+  #slides {
+    width: 70%;
+    height: 85%;
+  }
+}
+
+@media screen and (max-width:930px) {
+  #building {
+    height: 20em;
+  }
+
+  #slides {
+    width: 90%;
+    height: 100%;
+  }
+}
+
+@media screen and (max-width:800px) {
+  #building {
+    height: 17em;
+  }
+
+  #slides {
+    width: 100%;
+    height: 120%;
+  }
+}
+
+@media screen and (max-width:700px) {
+  #building {
+    display: none;
+  }
+
+  #person {
+    margin: 3%;
+  }
+
+  #title {
+    font-size: 12px;
+  }
+
+  #content {
+    font-size: 11px;
+  }
+}
+
+@media screen and (max-width:600px) {
+  #person {
+    width: 70%;
+  }
+}
+
+@media screen and (max-width:550px) {
+  #person {
+    width: 60%;
+  }
+
+  #content {
+    font-size: 10px;
+  }
+}
+
+@media screen and (max-width:450px) {
+  #person {
+    width: 50%;
+  }
+
+  #title {
+    font-size: 11px;
+  }
+
+  #content {
+    font-size: 8px;
+  }
+}
+
+@media screen and (max-width:400px) {
+  #person {
+    width: 40%;
+  }
+
+  #title {
+    font-size: 10px;
+  }
+
+  #content {
+    font-size: 7px;
+  }
+}
+
+@media screen and (max-width:360px) {
+  #person {
+    width: 30%;
+  }
+
+  #title {
+    font-size: 9px;
+  }
+
+  #content {
+    font-size: 6px;
+  }
 }
 </style>
