2e21d37413fca84a562dc5a8ea66679d1f55fc4e
Sanaa Syed <54586561+sanaasy@users.noreply.github.com>
2021-03-11 15:27:43 -0500

    Testimonial section (#13)
    * speakers tabs
    
    * Adding vueper slides
    
    * adding alt
    
    Co-authored-by: angel <li.angel.d02@gmail.com>
    Co-authored-by: Sanaa Syed <sanaa@Sanaas-MacBook-Pro.local>

--- src/components/homePage/Speakers.vue
+++ src/components/homePage/Speakers.vue
@@ -1,13 +1,26 @@
 <template>
 <div id="speakers">
 	<h2>CUTC 2020</h2>
-	<!-- TAB LAYOUT -->
+	<div id="TABS">
+		<p id="tab" @click="currentPage = 0"> Speakers </p>
+		<p id="tab" @click="currentPage = 1"> Events </p>
+	</div>
+	<div id="content">
+		<p v-if="currentPage == 0"> speaker info </p>
+		<p v-else> event info </p>
+	</div>
 </div>
 </template>
+
 
 <script>
 export default {
 	name: 'Speakers',
+	data () {
+		return {
+		currentPage: 0,
+		}
+	}
 }
 </script>
 
@@ -16,4 +29,22 @@ export default {
 	margin: 1em;
 	background: var(--nav-color);
 }
-</style>
+#TABS {
+	margin: 0 auto;
+	display: flex;
+}
+#content {
+	color: white;
+	padding: 40px;
+	display: flex;
+}
+#tab {
+	overflow: hidden;
+	display: flex;
+	padding: 40px;
+	cursor: pointer;
+	margin: 0 auto;
+	text-align: center;
+	float: center;
+}
+ </style>
