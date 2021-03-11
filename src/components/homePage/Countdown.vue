459da09e5979f98a46eba27946d65526511768f1
draymond63 <41529410+draymond63@users.noreply.github.com>
2021-03-10 22:17:08 -0500

    Added Countdown section to the Hero (#12)
    * Attempted countdown configuration
    
    * Fixed countdown positioning

--- src/components/homePage/Countdown.vue
+++ src/components/homePage/Countdown.vue
@@ -1,29 +1,70 @@
 <template>
 	<div id="countdown">
-		{{ timeStamp }}
+		<p class="title">COUNTDOWN!</p>
+		<div class="data">
+		<p>{{days}}</p>
+		<p>{{hours}}</p>
+		<p>{{minutes}}</p>
+		<p>{{seconds}}</p>
+		<p>D</p><p>H</p><p>M</p><p>S</p>
+		</div>
 	</div>
 </template>
 
 <script>
+const conferenceDate = new Date('July 24, 2021 10:00:00');
 export default {
 	name: 'Countdown',
 	data: () => ({
-		timeStamp: 0
+		days: 0,
+		hours: 0,
+		minutes: 0,
+		seconds: 0,
 	}),
 	methods: {
 		updateTimeStamp() {
-			// Date objects...
+			const now = Date.now();
+			const diff = new Date(conferenceDate - now);
+			this.days = Math.round(diff / 1000 / 60 / 60 / 24);
+			this.hours = diff.getHours();
+			this.minutes = diff.getMinutes();
+			this.seconds = diff.getSeconds();
 		}
+	},
+	beforeMount() {
+		setInterval(this.updateTimeStamp, 1000);
 	}
 }
 </script>
 
 <style scoped>
 #countdown {
-	width: 100%;
-	padding: 1em;
-	box-sizing: border-box;
+	position: absolute;
+	bottom: 34%;
+	right: 36%;
+	padding: 0.5em;
 
-	border: 1px solid black;
+	background: #ffe8e8;
+	border-radius: 8px;
+
+	transform: translate(50%, 50%) skew(0, -19deg);
+	z-index: 1;
+}
+
+.data {
+	display: grid;
+	grid-template-columns: repeat(4, 1fr);
+}
+
+.title {
+	text-align: center;
+	font-weight: normal;
+}
+
+p {
+	margin: auto;
+	font-weight: bold;
+	font-size: 1em;
+	color: var(--nav-color);
 }
 </style>
--- src/components/homePage/Hero.vue
+++ src/components/homePage/Hero.vue
@@ -1,31 +1,31 @@
 <template>
 <div id="hero">
-	<div class="intro">
+	<div class="building">
+		<countdown/>
 		<img src="../../assets/building.svg" id="cutc-building-image">
-		<!-- TEXT COLUMN -->
-		<span>
-			<h3>Welcome to the 2021</h3>
-			<h1>Canadian Undergraduate Technology Conference</h1>
-			<!-- <h2>Building our Future</h2> -->
-			<div style="height: 1em"/>
-
-			<h3>Jul 24-25, 2021 • Virtual Event</h3>
-
-			<!-- Begin Mailchimp Signup Form -->
-			<form action="https://cutc.us10.list-manage.com/subscribe/post?u=73d566326ae6bb629b42fb5fd&amp;id=e28a4a51fe" method="post" 
-					target="_blank" novalidate>
-				<input type="email" name="EMAIL" placeholder="Enter your email for updates!" required>
-				<!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-				<div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_73d566326ae6bb629b42fb5fd_e28a4a51fe" tabindex="-1" value=""></div>
-				<input type="submit" value="Submit">
-			</form>
-			<!-- End Mailchimp Signup Form -->
-
-			<!-- LINKS -->
-			<a href="https://2020.cutc.ca" target="_blank">Check out CUTC 2020</a>
-		</span>
 	</div>
-	<countdown/>
+	<!-- TEXT COLUMN -->
+	<span>
+		<h3>Welcome to the 2021</h3>
+		<h1>Canadian Undergraduate Technology Conference</h1>
+		<!-- <h2>Building our Future</h2> -->
+		<div style="height: 1em"/>
+
+		<h3>Jul 24-25, 2021 • Virtual Event</h3>
+
+		<!-- Begin Mailchimp Signup Form -->
+		<form action="https://cutc.us10.list-manage.com/subscribe/post?u=73d566326ae6bb629b42fb5fd&amp;id=e28a4a51fe" method="post" 
+				target="_blank" novalidate>
+			<input type="email" name="EMAIL" placeholder="Enter your email for updates!" required>
+			<!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+			<div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_73d566326ae6bb629b42fb5fd_e28a4a51fe" tabindex="-1" value=""></div>
+			<input type="submit" value="Submit">
+		</form>
+		<!-- End Mailchimp Signup Form -->
+
+		<!-- LINKS -->
+		<a href="https://2020.cutc.ca" target="_blank">Check out CUTC 2020</a>
+	</span>
 </div>
 </template>
 
@@ -45,21 +45,22 @@ export default {
 </script>
 
 <style scoped>
-.intro {
+#hero {
 	display: grid;
 	grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
-	align-items: center;
+	/* align-items: center; */
 	column-gap: 1em;
 	padding: 10% var(--splash-page-inset);
 	box-sizing: border-box;
 }
+.building {
+	position: relative;
+	height: min-content;
+}
 #cutc-building-image {
 	max-height: 50vh;
+	width: 100%;
 	justify-self: end;
-}
-
-img {
-	max-width: 100%;
 }
 
 input[type="email"] {
