dc1f0a17bb64a808c0611f1c7627f5ea33a1070b
Adib234 <adibadib42@hotmail.com>
2021-03-19 17:57:33 -0400

    halfway done the issue with 3 quirks

---
+++ src/components/homePage/Contributors.vue
@@ -0,0 +1,69 @@
+<template>
+  <div id="contributors">
+    <h1>Created with love in 🇨🇦 by</h1>
+    <div class="gallery">
+      <div v-for="(member,index) in members.members" :key="member.picture">
+        <a @click="open(index)" style="  text-decoration: none;">
+          <div class="dialog" v-if="popUp===index">
+            <div class="name">{{member.picture.slice(0,member.picture.indexOf("."))}}</div>
+            <div class="emoji">{{member.emoji}}</div>
+            <div class="position">{{member.position}}</div>
+          </div>
+          <img class="image" :src="require(`@/assets/contributors/${member.picture}`)" />
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Members from "../../assets/contributors/names";
+export default {
+  name: "Contributors",
+  data() {
+    return { members: Members, popUp: -1 };
+  },
+  methods: {
+    open: function(popped) {
+      this.popUp = popped;
+      console.log(popped);
+    }
+  }
+};
+</script>
+
+<style scoped>
+#contributors {
+  color: black;
+  margin-top: 5rem;
+}
+.gallery {
+  display: flex;
+  flex-flow: row wrap;
+}
+.image {
+  max-height: 5rem;
+  margin-right: 2rem;
+  margin-bottom: 1rem;
+  border-top-left-radius: 50% 50%;
+  border-top-right-radius: 50% 50%;
+  border-bottom-right-radius: 50% 50%;
+  border-bottom-left-radius: 50% 50%;
+}
+.dialog {
+  padding-top: 0.2rem;
+  padding-left: 0.5rem;
+  width: 8rem;
+  height: 3.5rem;
+  color: black;
+  background: #b3160d;
+}
+.name {
+  color: white;
+  font-weight: bold;
+}
+.position {
+  color: white;
+  font-size: 0.8rem;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,34 +1,34 @@
 <template>
-<span style="scroll">
-	<div class="fade">
-		<nav-bar />
-		<hero />
-	</div>
+  <span style="scroll">
+    <div class="fade">
+      <nav-bar />
+      <hero />
+    </div>
 
-		<div class="home-content">
-		<!-- Timeline -->
-		<about />
-		<!-- Tab -->
-		<speakers />
-		<!-- Carousel -->
-		<testimonials />
+    <div class="home-content">
+      <!-- Timeline -->
+      <about />
+      <!-- Tab -->
+      <speakers />
+      <!-- Carousel -->
+      <testimonials />
 
-		<!--<div id="sponsors">
+      <!--<div id="sponsors">
 			<h2>Our 2020 Sponsors and Partners</h2>
 			<img
 				src="../assets/sponsors2020.png"
 				alt="Flik, Geotab, lleTTonna, Flipp, FoundersBeta, RBC, jack.org, JobJunxion"
 			/>
-		</div>-->
+      </div>-->
 
-		<!-- Dropdown -->
-		<faq />
-		<!-- Unsure? -->
-		<!-- <team /> -->
-
-	</div>
-	<bottom />
-</span>
+      <!-- Dropdown -->
+      <faq />
+      <!-- Unsure? -->
+      <!-- <team /> -->
+      <contributors />
+    </div>
+    <bottom />
+  </span>
 </template>
 
 <script>
@@ -39,51 +39,53 @@ import Speakers from "../components/homePage/Speakers.vue";
 import Testimonials from "../components/homePage/Testimonials.vue";
 import Faq from "../components/homePage/Faq.vue";
 // import Team from "../components/homePage/Team.vue";
+import Contributors from "../components/homePage/Contributors.vue";
 import Bottom from "../components/homePage/Footer.vue";
 export default {
-	name: "Home",
-	components: {
-		NavBar,
-		Hero,
-		About,
-		Speakers,
-		Testimonials,
-		Faq,
-		// Team,
-		Bottom
-	}
+  name: "Home",
+  components: {
+    NavBar,
+    Hero,
+    About,
+    Speakers,
+    Testimonials,
+    Faq,
+    // Team,
+    Contributors,
+    Bottom
+  }
 };
 </script>
 
 <style scoped>
 .home-content {
-	width: 80%;
-	margin: auto;
+  width: 80%;
+  margin: auto;
 }
 
 .fade {
-	background: linear-gradient(
-		180deg,
-		#ff4e4e 0%,
-		#ffaeae 66.67%,
-		#ffc5c5 77.6%,
-		#ffd8d8 87.5%,
-		#ffffff 100%
-	);
+  background: linear-gradient(
+    180deg,
+    #ff4e4e 0%,
+    #ffaeae 66.67%,
+    #ffc5c5 77.6%,
+    #ffd8d8 87.5%,
+    #ffffff 100%
+  );
 }
 
-#sponsors>img {
-	width: 90%;
-	margin: auto;
+#sponsors > img {
+  width: 90%;
+  margin: auto;
 }
 
 .footer {
-	display: flex;
-	align-items: center;
-	padding: 1% 0;
-	background: var(--nav-color);
+  display: flex;
+  align-items: center;
+  padding: 1% 0;
+  background: var(--nav-color);
 }
 .footer > a {
-	padding-left: 2em;
+  padding-left: 2em;
 }
 </style>
