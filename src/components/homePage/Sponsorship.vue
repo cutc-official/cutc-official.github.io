602431cd6ea1a9f5d132fce7bd78b12d0fc63699
Daniel Raymond <dan@raymond.ch>
2021-03-27 21:43:06 -0400

    Remove dialogs since design doesn't want them

--- src/components/homePage/Sponsorship.vue
+++ src/components/homePage/Sponsorship.vue
@@ -7,22 +7,16 @@
 			alt="Flik, Geotab, lleTTonna, Flipp, FoundersBeta, RBC, jack.org, JobJunxion"
 		/>
 
-		<div
+		<a
 			v-for="(coords, name, i) in coordinates"
 			:key="name"
 			class="dot"
 			:style="{'--dot-order': i, 'left': coords[0], 'top': coords[1]}"
-			@mouseover="() => showSponsor(name)" 
-			@mouseleave="hideSponsor"
-		>
-			<div class="dialog-box" v-if="sponsor === name">
-				<b>{{name}}</b>
-				<br>
-				<a :href="info[name].url">{{ info[name].url }}</a>
-			</div>
-		</div>
-	</div>
+			:href="links[name]"
+			target="_blank">
+		</a>
 
+	</div>
 </div>
 </template>
 
@@ -33,29 +27,19 @@ export default {
 	name: 'Sponsorship',
 	data() {
 		return {
-			'info': sponsorData,
+			'links': sponsorData,
 			'coordinates': {
-				'Flik': ['12.3%', '41.3%'],
-				'Geotab': ['14.8%', '26.3%'],
-				'lleTTonna': ['29.8%', '38.0%'],
-				'Flipp': ['36.1%', '24.8%'],
-				'FoundersBeta': ['44.7%', '46.3%'],
-				'RBC': ['55.0%', '1.60%'],
-				'jack.org': ['70.8%', '27.3%'],
-				'JobJunxion': ['75.3%', '44.1%'],
-			},
-			'sponsor': null
+				'Flik': ['13.8%', '41.2%'],
+				'Geotab': ['16.5%', '26%'],
+				'lleTTonna': ['33.2%', '37.8%'],
+				'Flipp': ['40.1%', '24.6%'],
+				'FoundersBeta': ['49.8%', '46.1%'],
+				'RBC': ['61.3%', '1.4%'],
+				'jack.org': ['78.6%', '27.1%'],
+				'JobJunxion': ['83.7%', '43.9%'],
+			}
 		}
 	},
-
-	methods: {
-		showSponsor(sponsor) {
-			this.sponsor = sponsor;
-		},
-		hideSponsor() {
-			this.sponsor = null;
-		}
-	}
 }
 </script>
 
@@ -65,7 +49,7 @@ export default {
 }
 
 .sponsor-img > img {
-  width: 90%;
+  width: 100%;
   margin: auto;
 }
 
@@ -74,36 +58,22 @@ export default {
 	width: 1vw;
 	height: 1vw;
 	border-radius: 50em;
+	cursor: pointer;
 
 	background: white;
-	animation: bounce 5s forwards infinite;
-	animation-delay: calc(var(--dot-order) * 200ms);
+	animation: bounce 4s infinite;
+	animation-delay: calc(var(--dot-order) * 401ms);
 }
 
 @keyframes bounce {
 	from {
-		box-shadow: 0px 0px 3px 0px rgba(180, 110, 110, 0.5);
+		box-shadow: 0px 0px 3px 0px rgba(180, 110, 110, 0);
 	}
 	20% {
-		box-shadow: 0px 0px 3px 3px rgba(255, 100, 100, 0.25);
+		box-shadow: 0px 0px 3px 3px rgba(255, 100, 100, 0.4);
 	}
-	70% {
-		box-shadow: 0px 0px 3px 7px rgba(255, 100, 100, 0.05);
+	to {
+		box-shadow: 0px 0px 3px 9px rgba(255, 100, 100, 0);
 	}
-	100% {
-		box-shadow: 0px 0px 3px 5px rgba(255, 100, 100, 0);
-	}
-}
-
-.dialog-box {
-	width: max-content;
-	padding: 1em;
-
-	background: white;
-	border: 1px solid var(--main-color);
-	border-radius: 1em;
-}
-b, a {
-	color: grey;
 }
 </style>
