f13b21a3e05a2f9de7a0c27ed882688013d90f44
Daniel Raymond <dan@raymond.ch>
2021-05-08 08:56:17 -0700

    Blog launch prep

--- src/components/general/Blog.vue
+++ src/components/general/Blog.vue
@@ -46,7 +46,7 @@ export default {
 				referenceThis.blogData = JSON.parse(this.responseText);
 			}
 		};
-		httpRequest.open("GET", "http://test.cutc.ca/mediumPosts.php", true);
+		httpRequest.open("GET", "http://api.cutc.ca/mediumPosts.php", true);
 		httpRequest.send();
 	}
 }
