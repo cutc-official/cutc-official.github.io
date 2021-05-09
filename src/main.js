c91846f9b13979e6beb15d594806f4f22ec1fa99
Daniel Raymond <dan@raymond.ch>
2021-05-09 12:26:36 -0700

    Tried GA pageview support

--- src/main.js
+++ src/main.js
@@ -7,3 +7,13 @@ const app = createApp(App)
 app.use(router)
 // * Adds the app to the div with the id 'app'
 app.mount('#app')
+
+// Set up page tracking, because the router is in WebHashHistory mode (SPA)
+// https://developers.google.com/analytics/devguides/collection/analyticsjs/single-page-applications
+// ga('set', 'page', router.currentRoute.path);
+// ga('send', 'pageview');
+
+// router.afterEach((to) => {
+//   ga('set', 'page', to.path);
+//   ga('send', 'pageview');
+// });
