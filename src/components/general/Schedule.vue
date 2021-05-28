322dae78ae5622f80c0d9655bb7864d6942faf67
Adib234 <adibadib42@hotmail.com>
2021-05-27 23:11:28 -0400

    git retooling and minor css changes

--- src/components/general/Schedule.vue
+++ src/components/general/Schedule.vue
@@ -2,12 +2,11 @@
 	<h2>Schedule</h2>
 	<router-link v-if="link" to="/schedule">View full schedule ></router-link>
 	<div>
-		<schedule-tile v-bind="scheduleData['8:00'][0]"/>
+		<schedule-tile title="hi"/>
 	</div>
 </template>
 
 <script>
-import json from "@/content/schedule.json";
 import ScheduleTile from "@/components/general/ScheduleTile.vue";
 
 export default {
@@ -18,11 +17,6 @@ export default {
 	props: {
 		link: Boolean
 	},
-	data() {
-		return {
-			scheduleData: json
-		}
-	}
 }
 </script>
 
