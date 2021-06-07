3adb0e8b94d123d2ed6496c63c575c875f2c548b
Daniel Raymond <dan@raymond.ch>
2021-06-06 22:50:39 -0700

    Switched speaker orders

--- src/components/general/ScheduleTile.vue
+++ src/components/general/ScheduleTile.vue
@@ -148,7 +148,7 @@ export default {
 .calendar>.child {
   display: flex;
   align-items: center;
-  justify-content: start;
+  justify-content: flex-start;
 }
 .calendar>.child>.icon {
   width: 1rem;
