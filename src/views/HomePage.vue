c79c79eae40155ce9b2823522fefd3d7463f6933
Daniel Raymond <dan@raymond.ch>
2021-06-14 19:59:26 -0700

    Fixed indentation

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,23 +1,23 @@
 <template>
-  <span style="scroll" id="home-page">
-    <div class="fade">
-      <nav-bar :hasBackground="false" />
-      <hero />
-    </div>
+	<span style="scroll" id="home-page">
+	<div class="fade">
+		<nav-bar :hasBackground="false" />
+		<hero />
+	</div>
 
-    <div class="home-content">
-      <about />
-      <speakers :limit="8" />
-      <h4 class="schedule-placeholder">Schedule coming soon!</h4>
-      <!-- <schedule :link="true" /> -->
-      <testimonials />
-      <sponsorship />
-      <blog :limit="6" />
-      <faq />
-      <contributors />
-    </div>
-    <bottom />
-  </span>
+	<div class="home-content">
+		<about />
+		<speakers :limit="8" />
+		<h4 class="schedule-placeholder">Schedule coming soon!</h4>
+		<!-- <schedule :link="true" /> -->
+		<testimonials />
+		<sponsorship />
+		<blog :limit="6" />
+		<faq />
+		<contributors />
+	</div>
+	<bottom />
+	</span>
 </template>
 
 <script>
@@ -34,62 +34,61 @@ import Contributors from "@/components/homePage/Contributors.vue";
 import Bottom from "@/components/general/Footer.vue";
 
 export default {
-  name: "Home",
-  components: {
-    NavBar,
-    Hero,
-    About,
-    Speakers,
-    // Schedule,
-    Testimonials,
-    Sponsorship,
-    Blog,
-    Faq,
-    Contributors,
-    Bottom,
-  },
+	name: "Home",
+	components: {
+	NavBar,
+	Hero,
+	About,
+	Speakers,
+	// Schedule,
+	Testimonials,
+	Sponsorship,
+	Blog,
+	Faq,
+	Contributors,
+	Bottom,
+	},
 };
 </script>
-, Sponsorship
 
 <style>
 .home-content {
-  width: 80%;
-  max-width: 1200px;
-  margin: auto;
+	width: 80%;
+	max-width: 1200px;
+	margin: auto;
 }
 
 #home-page h2 {
-  margin-top: 2em;
+	margin-top: 2em;
 }
 
 .schedule-placeholder {
-  margin-top: 2em;
-  text-align: center;
-  color: black;
-  font-weight: bold;
+	margin-top: 2em;
+	text-align: center;
+	color: black;
+	font-weight: bold;
 }
 
 .fade {
-  background: linear-gradient(
-    180deg,
-    #ff4e4e 0%,
-    #ffaeae 66.67%,
-    #ffc5c5 77.6%,
-    #ffd8d8 87.5%,
-    #ffffff 100%
-  );
+	background: linear-gradient(
+	180deg,
+	#ff4e4e 0%,
+	#ffaeae 66.67%,
+	#ffc5c5 77.6%,
+	#ffd8d8 87.5%,
+	#ffffff 100%
+	);
 }
 @media screen and (max-width: 850px) {
-  .fade {
-    background: linear-gradient(
-      180deg,
-      #ff4e4e 5%,
-      #ffaeae 66.67%,
-      #ffc5c5 77.6%,
-      #ffd8d8 90%,
-      #ffffff 100%
-    );
-  }
+	.fade {
+	background: linear-gradient(
+		180deg,
+		#ff4e4e 5%,
+		#ffaeae 66.67%,
+		#ffc5c5 77.6%,
+		#ffd8d8 90%,
+		#ffffff 100%
+	);
+	}
 }
 </style>
