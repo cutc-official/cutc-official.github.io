54612798f6e54441b72471b97f6a4205f7ab1c10
Daniel Raymond <dan@raymond.ch>
2021-06-22 22:43:07 -0700

    New sponsors

--- src/components/homePage/Sponsorship.vue
+++ src/components/homePage/Sponsorship.vue
@@ -2,6 +2,8 @@
 <div id="sponsors">
 	<h2>Our 2021 Sponsors and Partners</h2>
 	<div class="mobile">
+		<a target="_blank" href="https://www.huawei.com/en/" class="gold">   <img src="@/assets/sponsors/huawei.png"    alt="Huawei"></a>
+		<a target="_blank" href="https://www.bdo.com"        class="gold">   <img src="@/assets/sponsors/bdo.png"       alt="BDO"></a>
 		<a target="_blank" href="https://www.geotab.com"     class="gold">   <img src="@/assets/sponsors/geotab.png"    alt="Geotab"></a>
 		<a target="_blank" href="https://www.shopify.com/"   class="silver"> <img src="@/assets/sponsors/shopify.png"   alt="Shopify"></a>
 		<a target="_blank" href="https://www.accenture.com/" class="silver"> <img src="@/assets/sponsors/accenture.png" alt="Accenture"></a>
@@ -9,7 +11,7 @@
 		<a target="_blank" href="http://copysmith.ai/"       class="partner"><img src="@/assets/sponsors/copysmith.png" alt="Copysmith"></a>
 	</div>
 	<div class="desktop">
-		<img src="@/assets/sponsors/desktop.png" alt="Geotab, Shopify, Accenture, Flipp, Copysmith"/>
+		<img src="@/assets/sponsors/desktop.png" alt="Huawei, BDO, Geotab, Shopify, Accenture, Flipp, Copysmith"/>
 		<div
 			v-for="(coords, name, i) in dotCoordinates"
 			:key="name"
@@ -39,22 +41,24 @@ export default {
 			links: sponsorData,
 			dotCoordinates: {
 				// '': ['13.8%', '41.2%'],
-				'Accenture': ['16.0%', '26.5%'],
-				'Flipp':     ['38.8%', '37.5%'],
-				// '': ['40.1%', '24.6%'],
-				'Copysmith': ['49.8%', '47%'],
-				'Geotab':    ['61.3%', '2%'],
-				'Shopify':   ['78.6%', '27.1%'],
+				'Accenture': ['23.0%', '23.5%'],
+				'Huawei':    ['37.1%', '10.5%'],
+				'Flipp':     ['38.8%', '35.5%'],
+				'Copysmith': ['49.8%', '44.6%'],
+				'Geotab':    ['51.3%', '2%'],
+				'BDO':       ['60.1%', '10.0%'],
+				'Shopify':   ['78.6%', '21.1%'],
 				// '': ['83.7%', '43.9%'],
 			},
 			linkCoordinates: {
 				// '':       {left: '6.80%', top: '40.2%', width: '10%',   height: '8%'},
-				'Accenture': {left: '13%',   top: '24.0%', width: '12%',   height: '10%'},
-				'Flipp':     {left: '31.0%', top: '36.0%', width: '10.4%', height: '8%'},
-				// '':       {left: '39.5%', top: '23.6%', width: '8.5%',  height: '7%'},
-				'Copysmith': {left: '43.3%', top: '45%',   width: '9%',    height: '8%'},
-				'Geotab':    {left: '48.5%', top: '0',     width: '16.5%', height: '13%'},
-				'Shopify':   {left: '69%',   top: '25.0%', width: '13%',   height: '10%'},
+				'Accenture': {left: '14%',   top: '21.0%', width: '11%',   height: '10%'},
+				'Huawei':    {left: '27.0%', top: '8.5%',  width: '13%',   height: '11%'},
+				'Flipp':     {left: '31.6%', top: '34.0%', width: '9%',    height: '8%'},
+				'Copysmith': {left: '43.5%', top: '43%',   width: '9%',    height: '8%'},
+				'Geotab':    {left: '42.5%', top: '0',     width: '13.5%', height: '11%'},
+				'BDO':       {left: '57.5%', top: '8.5%',  width: '13.5%', height: '11%'},
+				'Shopify':   {left: '71.4%', top: '19.0%', width: '12%',   height: '10%'},
 				// '':       {left: '78.1%', top: '42.9%', width: '12%',   height: '7%'},
 			}
 		}
@@ -74,6 +78,7 @@ img {
 }
 .link {
 	position: absolute;
+	/* border: 1px solid grey; */
 }
 
 .dot {
