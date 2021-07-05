cd7bff53db7bfdf3d61c6c6b59d478613f875a9d
Emily Louie <emilyslouie@gmail.com>
2021-07-05 08:47:56 -0400

    update students reached stat

--- src/components/homePage/About.vue
+++ src/components/homePage/About.vue
@@ -1,58 +1,73 @@
 <template>
-<div class="slider-wrapper">
-  <div class="slider" style="--animationDuration: 17s; --gradientWidth: 100px; --logoWH: 56px; --textSize: 1.5rem; --animationDirection: normal; --spacing: 1.5rem" :style="{'--numOfLogos': metrics.length}">
-    <div class="slide-track">
-      <template v-for="n in 3" :key="n">
-        <div
-          class="slide"
-          v-for="metric in metrics"
-          :key="metric"
-        >
-          <p class="slide-content">{{metric}}</p>
-        </div>
-      </template>
+  <div class="slider-wrapper">
+    <div
+      class="slider"
+      style="--animationDuration: 17s; --gradientWidth: 100px; --logoWH: 56px; --textSize: 1.5rem; --animationDirection: normal; --spacing: 1.5rem"
+      :style="{ '--numOfLogos': metrics.length }"
+    >
+      <div class="slide-track">
+        <template v-for="n in 3" :key="n">
+          <div class="slide" v-for="metric in metrics" :key="metric">
+            <p class="slide-content">{{ metric }}</p>
+          </div>
+        </template>
+      </div>
+    </div>
+    <div
+      class="slider"
+      style="--animationDuration: 14s; --gradientWidth: 100px; --logoWH: 56px; --animationDirection: reverse; --spacing: 1.5rem"
+      :style="{ '--numOfLogos': logos.length }"
+    >
+      <div class="slide-track">
+        <template v-for="n in 3" :key="n">
+          <div class="slide" v-for="logo in logos" :key="logo">
+            <img :src="getImage(logo)" class="logo" :alt="logo + ' logo'" />
+          </div>
+        </template>
+      </div>
     </div>
   </div>
-  <div class="slider" style="--animationDuration: 14s; --gradientWidth: 100px; --logoWH: 56px; --animationDirection: reverse; --spacing: 1.5rem" :style="{'--numOfLogos': logos.length}">
-    <div class="slide-track">
-      <template v-for="n in 3" :key="n">
-        <div
-          class="slide"
-          v-for="logo in logos"
-          :key="logo"
-        >
-          <img :src="getImage(logo)" class="logo" :alt="logo + ' logo'" />
-        </div>
-      </template>
-    </div>
-  </div>
-</div>
 </template>
 
 <script>
 export default {
-	data() {
-		return {
-			logos: ["amazon", "facebook", "github", "google", "notion", "rbc", "shopify", "stanford", "ycombinator"],
-			metrics: ["21st conference", "250+ featured industry leaders", "60+ speakers", "5000+ students reached", "30+ companies represented"]
-		};
-	},
-	methods: {
-		getImage(logo) {
-			try {
-				// speaker name is the exact same as the file path in speaker assets
-				return require(`@/assets/about/${logo}.png`);
-			} catch (e) {
-				throw Error(`pic does not exist: ${logo}`);
-				// image placeholder when name does not match the filename of a headshot
-			}
-		}
-	}
-}
+  data() {
+    return {
+      logos: [
+        "amazon",
+        "facebook",
+        "github",
+        "google",
+        "notion",
+        "rbc",
+        "shopify",
+        "stanford",
+        "ycombinator",
+      ],
+      metrics: [
+        "21st conference",
+        "250+ featured industry leaders",
+        "60+ speakers",
+        "10K+ students reached",
+        "30+ companies represented",
+      ],
+    };
+  },
+  methods: {
+    getImage(logo) {
+      try {
+        // speaker name is the exact same as the file path in speaker assets
+        return require(`@/assets/about/${logo}.png`);
+      } catch (e) {
+        throw Error(`pic does not exist: ${logo}`);
+        // image placeholder when name does not match the filename of a headshot
+      }
+    },
+  },
+};
 </script>
 
 <style scoped>
-
 /* shrink logo and gradient width on mobile */
 @media screen and (max-width: 850px) {
   .slider {
@@ -95,7 +110,8 @@ export default {
   color: black;
 }
 
-.slider::before, .slider::after {
+.slider::before,
+.slider::after {
   background: linear-gradient(to right, white 0%, rgba(255, 255, 255, 0) 100%);
   content: "";
   height: 100%;
@@ -115,8 +131,10 @@ export default {
 }
 
 .slider .slide-track {
-  -webkit-animation: scroll var(--animationDuration) linear infinite var(--animationDirection);
-          animation: scroll var(--animationDuration) linear infinite var(--animationDirection);
+  -webkit-animation: scroll var(--animationDuration) linear infinite
+    var(--animationDirection);
+  animation: scroll var(--animationDuration) linear infinite
+    var(--animationDirection);
   display: flex;
   position: absolute;
 }
@@ -127,8 +145,8 @@ export default {
 
 .slider .slide {
   height: var(--logoWH);
-	display: flex;
-	align-items: center;
+  display: flex;
+  align-items: center;
 }
 
 .slide-content {
@@ -141,5 +159,4 @@ export default {
   width: var(--logoWH);
   height: 100%;
 }
-
 </style>
