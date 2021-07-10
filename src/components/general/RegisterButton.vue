c7dbe6468a9eac0675b6de20cd1b462d7af9c159
Sanaa Syed <sanaa.syed@shopify.com>
2021-07-10 00:45:26 -0400

    Adding modal pop up

--- src/components/general/RegisterButton.vue
+++ src/components/general/RegisterButton.vue
@@ -4,35 +4,12 @@
 		class="register-button"
 		>REGISTER FOR FREE
 	</a> -->
-		
-	<a @onclick="openCheckout()" id="eventbrite-widget-modal-trigger-151149800121" rel="noopener noreferrer" target="_blank" class="register-button" >
-		Buy Tickets
-	</a>
+	<a id="eventbrite-widget-modal-trigger-151149800121" type="button" class="register-button">REGISTER FOR FREE</a>
 </template>
 
 <script>
 export default {
 	name: 'registerButton',
-	mounted() {
-		let recaptchaScript = document.createElement('script')
-		recaptchaScript.setAttribute('src', 'https://www.eventbrite.ca/static/widgets/eb_widgets.js')
-		document.head.appendChild(recaptchaScript)
-	},
-	methods: {
-		openCheckout() {
-			let exampleCallback = () =>{
-				console.log('Order complete!');
-			}
-			window.EBWidgets.createWidget({
-				widgetType: 'checkout',
-				eventId: '151149800121',
-				modal: true,
-				iframeContainerId: "example-widget-trigger",
-				modalTriggerElementId: 'eventbrite-widget-modal-trigger-151149800121',
-				onOrderComplete: exampleCallback
-			});
-		}
-	}
 }
 
 
