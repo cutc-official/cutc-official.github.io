113b2e00fcf68fdf892b28dfe0952ae6ee1b4281
JaydenHsiao <j3hsiao@edu.uwaterloo.ca>
2021-07-13 18:15:52 -0400

    load video component

---
+++ src/components/homePage/Video.vue
@@ -0,0 +1,21 @@
+<template>
+  <h2>Our Video</h2>
+  <video width="1920" height="1080" controls>
+    <source src="@/assets/misc/promo_video.mp4" type="video/mp4">
+  </video>
+</template>
+
+<script>
+export default {
+  name: "Video"
+}
+</script>
+
+<style scoped>
+  video {
+    margin: 0 auto;
+    display: block;
+    width: 80%;
+    height: auto;
+  }
+</style>
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -8,6 +8,7 @@
 		<div class="home-content">
 			<about />
 			<speakers :limit="8" />
+			<Video />
 			<schedule-preview />
 			<testimonials />
 			<sponsorship />
@@ -24,6 +25,7 @@ import NavBar from "@/components/general/NavBar.vue";
 import Hero from "@/components/homePage/Hero.vue";
 import About from "@/components/homePage/About.vue";
 import Speakers from "@/components/general/Speakers.vue";
+import Video from "@/components/homePage/Video.vue";
 import SchedulePreview from "@/components/homePage/SchedulePreview.vue";
 import Testimonials from "@/components/homePage/Testimonials.vue";
 import Sponsorship from "@/components/homePage/Sponsorship.vue";
@@ -39,6 +41,7 @@ export default {
 		Hero,
 		About,
 		Speakers,
+		Video,
 		SchedulePreview,
 		Testimonials,
 		Sponsorship,
